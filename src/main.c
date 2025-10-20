e40d333ae5d08d2f0f56120b5e9ca04d2944e457
viastolfi <vincent.astolfi12@gmail.com>
2025-10-20 15:30:58 +0200

    feat(frontend): add lexer_h lib and parse base clf file

--- src/main.c
+++ src/main.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define LEXER_LIB_IMPLEMENTATION
+#include "lexer.h"
 
-int main(void) 
+int main(int argc, char** argv) 
 {
-  printf("Hello World!\n");
+  if (argc < 2) {
+    fprintf(stderr, "ERROR - please provide a Cleaf file to compile\n");
+    return 1;
+  }
+
+  FILE *f = fopen(argv[1] ,"rb");
+  if (f == NULL) {
+    fprintf(stderr, "ERROR - can't open provided file\n"); 
+  }
+
+  char *text = (char *) malloc(1 << 20);
+  int len = f ? (int) fread(text, 1, 1<<20, f) : -1;
+  lexer_t lex;
+  if (len < 0) {
+    fprintf(stderr, "Error opening file\n");
+    free(text);
+    fclose(f);
+    return 1;
+  }
+  fclose(f);
+
+  lexer_init_lexer(&lex, text, text+len, (char*) malloc(4096), 4096);
+
+  while (lexer_get_token(&lex)) {
+    if (lex.token == LEXER_token_parse_error) {
+      printf("\n<<<PARSE ERROR>>>\n");
+      break;
+    }
+    lexer_print_token(&lex);
+    printf("  ");
+  }
+
+  free(text);
   return 0;
 }
